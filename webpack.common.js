3257698866e4ad16ebc3e666f5e64736b7d038bb
Aleksandr Shatskikh <work@errand.ru>
2022-03-21 11:12:55 +0300

    prod: heroku deploy

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin').default;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   target: 'web',
